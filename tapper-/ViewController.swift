1c6bfa151481a85f77f1d2719199b4a445a449ce
kanjiz1 <oforkanjiodekpe@Oforkanjis-MacBook-Pro.local>
2016-06-25 17:38:52 -0500

    First Version

--- tapper-/ViewController.swift
+++ tapper-/ViewController.swift
@@ -9,17 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Properties
+    var maxTaps: Int = 0;
+    var currentTaps : Int = 0;
+    
+    
+    
+    //Outlets
+    @IBOutlet weak var logoImage: UIImageView!;
+    @IBOutlet weak var howManyTapsTxt: UITextField!;
+    @IBOutlet weak var playButton: UIButton!;
+    
+    @IBOutlet weak var tapButton: UIButton!;
+    @IBOutlet weak var tapsLabel: UILabel!;
+    
+    @IBAction func onPlayButtonPressed( sender: UIButton!){
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != ""{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+            logoImage.hidden = true;
+            playButton.hidden = true;
+            howManyTapsTxt.hidden = true;
+            
+            tapButton.hidden = false;
+            tapsLabel.hidden = false;
+            
+            maxTaps = Int(howManyTapsTxt.text!)!;
+            currentTaps = 0;
+            updateTapsLabel();
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onStarTapped(sender: UIButton){
+        currentTaps++;
+        updateTapsLabel();
+        if isGameOver()
+        {
+            restartGame();
+        }
     }
-
-
+    
+    func updateTapsLabel(){
+        tapsLabel.text = "\(currentTaps) Taps";
+    }
+    
+    func isGameOver() -> Bool{
+        if currentTaps >= maxTaps{
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    func restartGame(){
+        maxTaps = 0;
+        howManyTapsTxt.text = "";
+        
+        logoImage.hidden = false;
+        playButton.hidden = false;
+        howManyTapsTxt.hidden = false;
+        
+        tapButton.hidden = true;
+        tapsLabel.hidden = true;
+    }
 }
-
